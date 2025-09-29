19abc358700672bf62851ee9afb5b318e7151f34
elvishrdz <elvishrdz@gmail.com>
2025-09-29 11:28:35 +0200

    add color scheme extended

--- charge/src/main/java/de/elvah/charge/platform/ui/theme/Theme.kt
+++ charge/src/main/java/de/elvah/charge/platform/ui/theme/Theme.kt
@@ -1,34 +1,15 @@
 package de.elvah.charge.platform.ui.theme
 
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = primaryDark,
-    secondary = secondaryDark,
-    background = containerDark,
-    surface = canvasDark,
-    tertiary = canvasDark,
-    onTertiary = containerDark
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = primary,
-    secondary = secondary,
-    background = container,
-    surface = canvas,
-    tertiary = container,
-    onTertiary = canvas
-)
+import de.elvah.charge.platform.ui.theme.colors.ElvahChargeColors
+import de.elvah.charge.platform.ui.theme.colors.LocalColorSchemeExtended
+import de.elvah.charge.platform.ui.theme.colors.brandColor
+import de.elvah.charge.platform.ui.theme.colors.onBrandColor
 
 @Composable
 internal fun ElvahChargeTheme(
@@ -36,21 +17,15 @@ internal fun ElvahChargeTheme(
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    CompositionLocalProvider(
+        LocalColorSchemeExtended provides ElvahChargeColors.getColorSchemeExtended(darkTheme),
+    ) {
+        MaterialTheme(
+            colorScheme = ElvahChargeColors.getColorScheme(darkTheme, dynamicColor),
+            typography = Typography,
+            content = content
+        )
     }
-
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
 
 internal val ColorScheme.brand: Color
---
+++ charge/src/main/java/de/elvah/charge/platform/ui/theme/colors/ElvahChargeColors.kt
@@ -0,0 +1,69 @@
+package de.elvah.charge.platform.ui.theme.colors
+
+import android.os.Build
+import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+
+internal object ElvahChargeColors {
+
+    private val LightColorScheme = lightColorScheme(
+        primary = primary,
+        secondary = secondary,
+        background = container,
+        surface = canvas,
+        tertiary = container,
+        onTertiary = canvas,
+    )
+
+    private val LightColorSchemeExtended = ColorSchemeExtended(
+        primary = primary,
+        decorativeStroke = decorativeStroke,
+        brand = brandColor,
+        onBrand = onBrandColor,
+        success = success,
+        onSuccess = onSuccess,
+    )
+
+    private val DarkColorScheme = darkColorScheme(
+        primary = primaryDark,
+        secondary = secondaryDark,
+        background = containerDark,
+        surface = canvasDark,
+        tertiary = canvasDark,
+        onTertiary = containerDark,
+    )
+
+    private val DarkColorSchemeExtended = ColorSchemeExtended(
+        primary = primary,
+        decorativeStroke = decorativeStroke,
+        brand = brandColor,
+        onBrand = onBrandColor,
+        success = success,
+        onSuccess = onSuccess,
+    )
+
+    @Composable
+    fun getColorScheme(darkTheme: Boolean, dynamicColor: Boolean): ColorScheme {
+        return when {
+            dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+                val context = LocalContext.current
+                if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            }
+
+            darkTheme -> DarkColorScheme
+            else -> LightColorScheme
+        }
+    }
+
+    fun getColorSchemeExtended(darkTheme: Boolean): ColorSchemeExtended {
+        return when {
+            darkTheme -> DarkColorSchemeExtended
+            else -> LightColorSchemeExtended
+        }
+    }
+}
