0024718205ae419be3c4148ba111d5fcb216d0fc
elvishrdz <elvishrdz@gmail.com>
2025-09-29 13:45:14 +0200

    remove unused variable

--- charge/src/main/java/de/elvah/charge/platform/ui/theme/Theme.kt
+++ charge/src/main/java/de/elvah/charge/platform/ui/theme/Theme.kt
@@ -1,14 +1,11 @@
 package de.elvah.charge.platform.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.graphics.Color
 import de.elvah.charge.platform.ui.theme.colors.ElvahChargeColors
 import de.elvah.charge.platform.ui.theme.colors.LocalColorSchemeExtended
-import de.elvah.charge.platform.ui.theme.colors.onBrandColor
 
 @Composable
 internal fun ElvahChargeTheme(
@@ -27,9 +24,5 @@ internal fun ElvahChargeTheme(
     }
 }
 
-internal val ColorScheme.onBrand: Color
-    get() = onBrandColor
-
 @Composable
 internal fun shouldUseDarkColors(darkTheme: Boolean?): Boolean = darkTheme ?: isSystemInDarkTheme()
-
