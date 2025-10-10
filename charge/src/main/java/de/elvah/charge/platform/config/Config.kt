072ebef0a92c7e5e95326a825dacb02e157bd5d8
Juan Jesús Cilla Ugarte <juanje.cilla@gmail.com>
2025-10-10 09:39:32 +0200

    Hotfix -  Release 0.3.5 (#28)
    * feat: Check api key config values for the different endpoints
    
    * Set auto-selection based on api key (#25)
    
    * Release version 0.3.5

--- charge/src/main/java/de/elvah/charge/platform/config/Config.kt
+++ charge/src/main/java/de/elvah/charge/platform/config/Config.kt
@@ -1,7 +1,51 @@
 package de.elvah.charge.platform.config
 
-class Config(
-    val apiKey: String,
-    val darkTheme: Boolean? = null,
-    val environment: Environment = Environment.Int,
-)
+import de.elvah.charge.platform.simulator.domain.model.SimulatorFlow
+
+public class Config(
+    public val apiKey: String,
+    public val darkTheme: Boolean? = null,
+    environment: Environment? = null,
+) {
+    public val environment: Environment = environment ?: detectEnvironmentFromApiKey(apiKey)
+    
+    init {
+        validateApiKey()
+    }
+
+    private fun validateApiKey() {
+        if (apiKey.isEmpty()) {
+            throw IllegalArgumentException("API key cannot be empty")
+        }
+
+        when (this.environment) {
+            Environment.Int -> if (!apiKey.startsWith("evpk_test")) {
+                if (apiKey.startsWith("evpk_prod")) {
+                    throw IllegalArgumentException("API Key error: You are using a production API key")
+                } else {
+                    throw IllegalArgumentException("API key must start with evpk_test")
+                }
+            }
+
+            Environment.Production -> {
+                if (!apiKey.startsWith("evpk_prod")) {
+                    if (apiKey.startsWith("evpk_test")) {
+                        throw IllegalArgumentException("API Key error: You are using a test API key")
+                    } else {
+                        throw IllegalArgumentException("API key must start with evpk_prod")
+                    }
+                }
+            }
+
+            else -> {}
+        }
+    }
+
+    private fun detectEnvironmentFromApiKey(apiKey: String): Environment {
+        return when {
+            apiKey.startsWith("evpk_test") -> Environment.Int
+            apiKey.startsWith("evpk_prod") -> Environment.Production
+            else -> Environment.Simulator(SimulatorFlow.Default) // Default fallback
+        }
+    }
+}
