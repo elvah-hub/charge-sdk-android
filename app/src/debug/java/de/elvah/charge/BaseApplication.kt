85926172dae0cd431e2b691d4d0454be1edfbab8
elvishrdz <elvishrdz@gmail.com>
2025-10-13 14:33:05 +0200

    refactor demo app init

---
+++ app/src/debug/java/de/elvah/charge/BaseApplication.kt
@@ -0,0 +1,22 @@
+package de.elvah.charge
+
+import android.app.Application
+import de.elvah.charge.platform.network.CustomNetworkInterceptorManager
+import de.elvah.charge.platform.network.NetworkRequestStorageManager
+import de.elvah.charge.sdk.initializeSdk
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        // Register your custom interceptor BEFORE initializing the SDK
+        val customLoggingInterceptor = CustomLoggingInterceptor()
+        CustomNetworkInterceptorManager.registerInterceptor(customLoggingInterceptor)
+
+        // Register your custom storage interceptor BEFORE initializing the SDK
+        val dummyInterceptor = DummyInterceptor()
+        NetworkRequestStorageManager.registerStorageInterceptor(dummyInterceptor)
+
+        initializeSdk()
+    }
+}
--- app/src/release/java/de/elvah/charge/BaseApplication.kt
+++ app/src/release/java/de/elvah/charge/BaseApplication.kt
@@ -1,14 +1,15 @@
 package de.elvah.charge
 
 import android.app.Application
-import de.elvah.charge.platform.config.Config
 import de.elvah.charge.platform.network.CustomNetworkInterceptorManager
 import de.elvah.charge.platform.network.NetworkRequestStorageManager
+import de.elvah.charge.sdk.initializeSdk
 
-class AppClass : Application() {
+class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
+        // TODO: this should be unaccessible
         // Register your custom interceptor BEFORE initializing the SDK
         val customLoggingInterceptor = CustomLoggingInterceptor()
         CustomNetworkInterceptorManager.registerInterceptor(customLoggingInterceptor)
@@ -17,11 +18,6 @@ class AppClass : Application() {
         val dummyInterceptor = DummyInterceptor()
         NetworkRequestStorageManager.registerStorageInterceptor(dummyInterceptor)
 
-        Elvah.initialize(
-            this,
-            Config(
-                "evpk_test_Syx9tZW1LGcIA7Js0BADiFg7HVDpUGk2CDYPd6zLKOqrSfEI0GIVzInH7W4WfoATHcPnEW7O3uP0GrsGL0IpeUjBf72BuYwanBJ4EUZTv",
-            ),
-        )
+        initializeSdk()
     }
 }
