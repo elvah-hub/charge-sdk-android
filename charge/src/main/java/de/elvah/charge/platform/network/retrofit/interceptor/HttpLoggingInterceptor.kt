ffa3455b60ec0dd53208ac28edfa2a2bcda35307
Juanje Cilla <juanje.cilla@gmail.com>
2025-11-06 16:08:45 +0100

    Disable logging for release

--- charge/src/main/java/de/elvah/charge/platform/network/retrofit/interceptor/HttpLoggingInterceptor.kt
+++ charge/src/main/java/de/elvah/charge/platform/network/retrofit/interceptor/HttpLoggingInterceptor.kt
@@ -1,8 +1,13 @@
 package de.elvah.charge.platform.network.retrofit.interceptor
 
+import de.elvah.charge.BuildConfig
 import okhttp3.logging.HttpLoggingInterceptor
 
 internal fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
     HttpLoggingInterceptor().apply {
-        level = HttpLoggingInterceptor.Level.BODY
+        level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
     }
