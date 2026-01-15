93316fa3ac3089d61f8f790423e06b5914225455
Juanje Cilla <juanje.cilla@gmail.com>
2026-01-15 15:04:35 +0100

    Add linting steps

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,4 +10,22 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.jetbrains.kotlin.jvm) apply false
     alias(libs.plugins.version.catalog.linter)
+    alias(libs.plugins.detekt)
+}
+
+allprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+}
+
+subprojects {
+    detekt {
+        buildUponDefaultConfig = true
+        allRules = false
+        config.setFrom("$rootDir/detekt.yml")
+        baseline = file("$rootDir/detekt-baseline.xml")
+    }
+
+    dependencies {
+        detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.7")
+    }
 }
